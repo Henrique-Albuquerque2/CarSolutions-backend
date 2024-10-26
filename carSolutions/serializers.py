38815aae9e2c2c41885ee5a02488f3b669adab52
Henrique Albuquerque <henriqueaa1@al.insper.edu.br>
2024-10-26 03:36:01 -0300

    Login, Logout e Cadastre-se integrados com front

--- carSolutions/serializers.py
+++ carSolutions/serializers.py
@@ -3,6 +3,7 @@ from carSolutions.models import User, Profile
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,14 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
                    "nacionalidade", "cep", "numero", "complemento", "cidade", 
                    "estado", "rua", "bairro", "isfuncionario",'genero']
 
-class MyTokenObtainPairSerializer (TokenObtainPairSerializer):
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
 
+        # Adiciona todas as informações do usuário ao token
         token["username"] = user.username
         token["email"] = user.email
-        token["isfuncionario"] = user.isfuncionario
         token["full_name"] = user.full_name
         token["cpf"] = user.cpf
         token["celular"] = user.celular
@@ -30,44 +31,51 @@ class MyTokenObtainPairSerializer (TokenObtainPairSerializer):
         token["estado"] = user.estado
         token["rua"] = user.rua
         token["bairro"] = user.bairro
+        token["isfuncionario"] = user.isfuncionario
         token["genero"] = user.genero
 
         return token
 
-    
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2", "full_name", "cpf", "celular", "nacionalidade", "cep", "numero", "complemento", "cidade", "estado", "rua", "bairro", "isfuncionario", "genero"]
+        fields = ["username", "email", "password", "password2", 
+                  "full_name", "cpf", "celular", "nacionalidade", 
+                  "cep", "numero", "complemento", "cidade", "estado",
+                    "rua", "bairro", "isfuncionario", "genero"]
     
+    def validate(self, attrs):
+        # Verifica se as senhas são iguais
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Senhas não são iguais."})
+        return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            full_name=validated_data["full_name"],
-            cpf=validated_data["cpf"],
-            celular=validated_data["celular"],
-            nacionalidade=validated_data["nacionalidade"],
-            cep=validated_data["cep"],
-            numero=validated_data["numero"],
-            complemento=validated_data["complemento"],
-            cidade=validated_data["cidade"],
-            estado=validated_data["estado"],
-            rua=validated_data["rua"],
-            bairro=validated_data["bairro"],
-            isfuncionario=validated_data["isfuncionario"],
-            genero=validated_data["genero"]
-        )
+        # Remove password2, pois não é necessário para criar o usuário
+        validated_data.pop("password2")
+        
+        # Criação do usuário com a senha validada
+        user = User.objects.create_user(**validated_data)
+        
+        # Define a senha usando set_password
         user.set_password(validated_data["password"])
         user.save()
 
         return user
-    
-def validate_password(self, attrs):
-    if attrs["password"] != attrs["password2"]:
-        raise serializers.ValidationError({"password": "Senhas não são iguais."})
-    return attrs
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.refresh_token = attrs["refresh"]
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            token = RefreshToken(self.refresh_token)
+            token.blacklist()  # Adiciona o token na lista negra
+        except Exception as e:
+            raise serializers.ValidationError("Token inválido ou já expirado.")
--- carSolutions/views.py
+++ carSolutions/views.py
@@ -2,17 +2,49 @@ from django.shortcuts import render
 
 # Create your views here.
 from carSolutions.models import User, Profile
-from carSolutions.serializers import UserSerializer, RegisterSerializer, MyTokenObtainPairSerializer
+from carSolutions.serializers import UserSerializer, RegisterSerializer, MyTokenObtainPairSerializer, LogoutSerializer
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # Obtem o usuário autenticado a partir dos dados validados
+        user = serializer.user
+
+        # Gera o token e obtém a resposta padrão com os tokens
+        response = super().post(request, *args, **kwargs)
+
+        # Adiciona informações adicionais ao corpo da resposta JSON
+        response.data.update({
+            "full_name": user.full_name,
+            "username": user.username,
+            "email": user.email,
+            "cpf": user.cpf,
+            "celular": user.celular,
+            "nacionalidade": user.nacionalidade,
+            "cep":user.cep,
+            "numero":user.numero,
+            "complemento": user.complemento,
+            "cidade": user.cidade,
+            "estado": user.estado,
+            "rua": user.rua,
+            "bairro": user.bairro,
+            "isfuncionario": user.isfuncionario,
+            "genero": user.genero
+        })
+        return response
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
@@ -32,3 +64,11 @@ def dashboard(request):
     
     return Response({'response': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
     
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = LogoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "Logout realizado com sucesso!"}, status=status.HTTP_205_RESET_CONTENT)
